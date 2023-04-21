63fc3757a9de8e5263aaa52ad2c2d5dde154b230
cynthia41 <cynthiaadhiambo41@gmail.com>
2023-04-21 17:59:34 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MovieCard from "./MovieCard";
 
+
 function App() {
   const title = "Mad Max";
   const posterURL =
@@ -9,7 +10,7 @@ function App() {
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title} img={posterURL} genres={genresArr} />
     </div>
   );
 }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,28 @@
 import React from "react";
 
+function App() {
+  const title = "Mad Max";
+  const posterURL =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
+  const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
+
+  return (
+    <div className="App">
+      <MovieCard title={title} img={posterURL} genres={genresArr} />
+    </div>
+  );
+
+  function MovieCard(props) {
+    return (
+      <div className="movie-card">
+        <img src={props.posterSrc} alt={props.title} />
+        <h2>{props.title}</h2>
+        <small>{props.genres.join(", ")}</small>
+      </div>
+    );
+    }
+}
+
 function MovieCard({
   title,
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
@@ -13,5 +36,27 @@ function MovieCard({
     </div>
   );
 }
+  // function MovieCard(props) {
+  //   return (
+  //     <div className="movie-card">
+  //       <img src={props.posterSrc} alt={props.title} />
+  //       <h2>{props.title}</h2>
+  //       <small>{props.genres.join(", ")}</small>
+  //     </div>
+  //   );
+  //   }
+  //   function MovieCard({ title, posterSrc, genres }) {
+  //     return (
+  //       <div className="movie-card">
+  //         <img src={posterSrc} alt={title} />
+  //         <h2>{title}</h2>
+  //         <small>{genres.join(", ")}</small>
+  //       </div>
+  //     );
+  //   }
+
+  
+
+
 
 export default MovieCard;
